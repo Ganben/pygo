e1e069d331562300bbd33eaff101e893ac3a33ae
Ganben <caozhao@gmail.com>
2016-09-08 01:46:31 +0800

    new models

--- park/models.py
+++ park/models.py
@@ -1,5 +1,29 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+from django.utils import timezone
+import datetime
 # Create your models here.
+
+@python_2_unicode_compatible
+class Car(models.Model):
+    name = models.CharField(max_length=20)
+
+
+@python_2_unicode_compatible
+class Ticket(models.Model):
+    car = models.ForeignObject(Car, validators=True)    #any way, many to one? not sure;
+    starttime = models.DateTimeField('start')
+    zone = models.DecimalField()   #GPS
+    fee_rate = models.IntegerField()
+    duration = models.DurationField()  #can be extended via extend
+    charger = models.CharField(max_length=10)   #who created
+    ispaid = models.BooleanField()    # if is paid
+    # billing = models.ManyToOneRel(Billing, validate=False)   #if paid, the billing must match? problem to paid.
+
+class Billing(models.Model):
+    amount = models.IntegerField
+    ticket = models.ForeignKey(Ticket)
+    transaction = models.CharField(max_length=40)
--- park/urls.py
+++ park/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'park'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^login/', views.login, name='login'),
-    url(r'^add/', views.add, name='add'),
-    url(r'^end/', views.end, name='end'),
-
+    url(r'^$', views.index, name='index'),   #index is button to check, list to charge, free space to public
+    url(r'^login/', views.login, name='login'),   #fake login page
+    url(r'^check/', views.add, name='check'),  #check picture to get status, paid or new parking ticket
+    url(r'^results/', views.end, name='results'),   #list of parking ticket, paid or not paid and end time, can be charge
+    url(r'^user/', views.user, name='user'),    #user index page, a list of numbers
+    url(r'^ticket/', views.ticket, name='ticket'),  #click car number to create a new ticket, and areas, can extend
 ]
