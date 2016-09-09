4082a2d948ae7fe0adea02567db7f7d3878f2ad2
Ganben <caozhao@gmail.com>
2016-09-09 23:54:30 +0800

    more views

--- park/urls.py
+++ park/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 
 from . import views
+from django.views.generic import TemplateView    #TemplateView.as_view(template_name="about.html")
 
 app_name = 'park'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),   #index is button to check, list to charge, free space to public
-    url(r'^login/', views.login, name='login'),   #fake login page
+    url(r'^login/', views.LoginView.as_view(), name='login'),   #fake login page
     url(r'^check/', views.add, name='check'),  #check picture to get status, paid or new parking ticket
     url(r'^results/', views.end, name='results'),   #list of parking ticket, paid or not paid and end time, can be charge
     url(r'^user/', views.user, name='user'),    #user index page, a list of numbers
--- park/views.py
+++ park/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views import View
+from .models import Car
 # Create your views here.
+
+class IndexView(ListView):
+    model = Car
+
+    def head(self, *args, **kwargs):
+        car_list = self.get_queryset()._fetch_all()
+        response = HttpResponse('')
+
+        response['car_list'] = car_list
+        return response
+
+
+class LoginView(View):
+    form_class = LoginForm
+    initial = {'key': 'value'}
+    template_name = 'form_template.html'
+
+    def get(self, request, *args, **kwargs):
+        #write some query
+        return render(request, 'form_template.html', {'form': form_class})
+
+    def post(self, request, *args, **kwargs):
+
+        return render(request, 'result')
+
