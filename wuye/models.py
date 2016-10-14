ba6d9a551ab3f1f40adeb385e22bd1258cbced45
ganben <caozhao@gmail.com>
2016-10-14 22:01:06 +0800

    new estate list proj for wj

---
+++ wuye/models.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.db import models
+# gen a house item list page; contains normal tags of a immobile assets.
+# think about management roles;
+
+# Create your models here.
